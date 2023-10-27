6036dd12b087697b11ed33795489bf080fe38400
wagngomes gomes <wagner.gomes.cx2@gmail.com>
2023-10-27 15:21:17 +0000

    sidebar iniciada

--- wagner-commerce/src/components/ui/header.tsx
+++ wagner-commerce/src/components/ui/header.tsx
@@ -1,19 +1,52 @@
+import { Span } from "next/dist/trace";
 import { Button } from "./button"
 import { Card, CardContent } from "./card"
-import { MenuIcon, ShoppingCartIcon } from "lucide-react"
+import { HomeIcon, ListOrderedIcon, LogInIcon, MenuIcon, PercentCircleIcon, PercentIcon, ShoppingCartIcon } from "lucide-react"
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 const Header = () => {
   return (
     <Card className="flex justify-between p-[1.875rem]">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">Menu</SheetHeader>
+          <div className="mt-2 flex flex-col gap-2">
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <LogInIcon size={16} />
+              Fazer login
+            </Button>
 
-        <Button size="icon" variant="outline">
-          <MenuIcon />
-        </Button>
-        <h1>FSW STORE</h1>
-        <Button size="icon" variant="outline">
-          <ShoppingCartIcon />
-        </Button>
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <HomeIcon size={16} />
+              Inicio
+            </Button>
 
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <PercentIcon size={16} />
+              Ofertas
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <ListOrderedIcon size={16} />
+              Catalogo
+            </Button>
+
+          </div>
+          
+        </SheetContent>
+      </Sheet>
+
+      <h1 className="text-lg font-semibold">
+        <span className="text-primary">FSW </span>STORE
+      </h1>
+      <Button size="icon" variant="outline">
+        <ShoppingCartIcon />
+      </Button>
     </Card>
   );
 };
